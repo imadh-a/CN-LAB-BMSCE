0971380d808bcf1676e7d35dad4ffc932ff8ae2e
imadh-a <93546632+imadh-a@users.noreply.github.com>
2023-01-05 19:21:26 +0530

    Update leaky_bucket.c

--- Week-8/leaky_bucket.c
+++ Week-8/leaky_bucket.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 int main() {
-  int capacity=0,packet=0,bsize=0,rate=0;
-    char ans='y';
-     printf("enter the bucket size : ");
+  int packet=0,bsize=0,rate=0;
+  int capacity=0;
+  char ans='y';
+  
+    printf("enter the bucket capacity : ");
     scanf("%d",&capacity);
     printf("enter the leaking rate : ");
     scanf("%d",&rate);
-      while(ans=='y')
- {
+    while(ans=='y')
+   {
         printf("\nenter the packet size : ");
         scanf("%d",&packet);
+        
          if((bsize+packet) > capacity)
-        {
-            printf("\n buffer full at the moment ");
+        {  
+            printf("\nbuffer full at the moment ");
         }
         
         else if((bsize+packet) <= capacity)
         {
             bsize+=packet;
+          
         }
         bsize-=rate;
-        printf("remaining bucket capacity is %d",bsize);
+        printf("\nremaining bucket capacity is %d",bsize);
         printf("\ndo you wish to keep adding packets? y/n : ");
         scanf("%s",&ans);
     
 }
+return 0;
 }
